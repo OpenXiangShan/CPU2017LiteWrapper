c966b7b306d8b484c2c6aee72bca17671e1f2379
jiaxiaoyu <xyyy1420@gmail.com>
2024-06-13 11:22:19 +0800

    Makefile: fix using rv64gcb collect.sh: when target dir exists, exit script. donot copy the suffix _r
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- scripts/collect.sh
+++ scripts/collect.sh
@@ -7,8 +7,17 @@ fi
 
 toolchain_name=$1
 
-# Create a directory to store the build results
-mkdir -p cpu2017_build.${toolchain_name}
+# Directory to store the build results
+target_dir="cpu2017_build.${toolchain_name}"
+
+# Check if the target directory already exists
+if [ -d "$target_dir" ]; then
+    echo "Target directory $target_dir already exists. Exiting."
+    exit 1
+fi
+
+# Create the target directory
+mkdir -p "$target_dir"
 
 # Loop over the build directories found by the find command
 find . -name "build" | while read build_dir; do
@@ -16,16 +25,14 @@ find . -name "build" | while read build_dir; do
     benchmark_name=$(basename $(dirname "$build_dir") | sed 's/^[0-9]*\.//')
     
     # Construct the desired name for the target file
-    new_name="${benchmark_name}"
+    new_name=$(echo "$benchmark_name" | sed 's/_r$//')
     
     # Identify the target file. We're assuming it's the only non-directory item directly under the build directory.
     target_file=$(find "$build_dir" -maxdepth 1 -type f ! -name "*.log")
     
-    # If target_file isn't empty, copy it to the cpu2006_build directory with the new name
+    # If target_file isn't empty, copy it to the cpu2017_build directory with the new name
     if [ -n "$target_file" ]; then
-        echo copying $target_file to cpu2017_build.${toolchain_name}/$new_name
-        cp "$target_file" "cpu2017_build.${toolchain_name}/$new_name"
+        echo copying $target_file to $target_dir/$new_name
+        cp "$target_file" "$target_dir/$new_name"
     fi
 done
-
-
